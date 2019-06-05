fc3a7886aec85468c84f9ebfee654365c96fe81f
zhilongwang <njuwangzhilong@gmail.com>
2019-06-05 16:33:25 +0800

    add all

--- test/exam1.c
+++ test/exam1.c
@@ -40,30 +40,34 @@ int padding[16];
 int arry[100];
 int data1,data2;
 int main(){
+	printf("arry address:%x\n",&arry[0]);
 	int i,j,k;
-	char input[30];
+	char input[20];
 	readstr(input);
 	data1=myatoi(input);
 	readstr(input);
 	data2=myatoi(input);
-	for(i=2;i<(data1%8);i++){
+	for(i=4;i<8;i++){
 		arry[i]=data1;
 	}
-	for(i=2;i<(data1%8);i=i+2){
+	for(i=4;i<8;i=i+2){
 		arry[i]=10;
 	}
-	for(i=8;i<8+(data2%8);i=i+2){
+	for(i=8;i<12;i=i+2){
 		arry[i]=data1*10;
 	}
-	for(i=16;i<24;i=i+3){
+	for(i=12;i<16;i=i+3){
 		arry[i]=data1%10;
+	}
+	for(i=16;i<20;i++){
+		arry[i]=4+data2;
 	}
 	data1=12;
 	data2=11;
-	for(i=2;i<24;i=i+4){
+	for(i=20;i<24;i++){
 		arry[i]=data1+data2;
 	}
-	char *r= (char *)(&data1);
+	char *r=&data1;
 	char *q=r;
 	char *p=q;
 	*(p+3)=(char)arry[5];
